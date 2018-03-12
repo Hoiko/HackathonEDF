4b75ea6e552ffdea7c7c3cef7edaddc9a44f3fd6
rodrig_8 <hugo.rodriguez@epitech.eu>
2018-03-12 16:27:47 +0100

    entity-mapping-bundles

---
+++ API/src/AppBundle/Entity/Answer.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Answer
+ *
+ * @ORM\Table(name="answer")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AnswerRepository")
+ */
+class Answer
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="answer", type="string", length=255)
+     */
+    private $answer;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set answer
+     *
+     * @param string $answer
+     *
+     * @return Answer
+     */
+    public function setAnswer($answer)
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    /**
+     * Get answer
+     *
+     * @return string
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+}
+
